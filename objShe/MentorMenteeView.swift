bae0f0c7fa7af319ef92fc77a44d75b70fb21496
gdomergue <gdomergu@callutheran.edu>
2016-07-23 15:53:45 -0700

    Added MentorMenteeView

---
+++ objShe/MentorMenteeView.swift
@@ -0,0 +1,43 @@
+//
+//  MentorMenteeView.swift
+//  objShe
+//
+//  Created by Gina Domergue on 7/23/16.
+//  Copyright © 2016 Yoko Yamaguchi. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class MentorMenteeView: UIView {
+
+    @IBOutlet weak var FindAMentor: UIButton!
+    @IBOutlet weak var BeAMentor: UIButton!
+    var view:UIView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        FindAMentor.addTarget(self, action: "Find a Mentor tapped", forControlEvents: UIControlEvents.TouchUpInside)
+        BeAMentor.addTarget(self, action: "Be a Mentor tapped", forControlEvents: UIControlEvents.TouchUpInside)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    func setup() {
+        view = loadViewFromNib()
+        view.frame = bounds
+//        view.autoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeightaddSubview(view)
+    }
+    
+    func loadViewFromNib() -> UIView {
+        let bundle = NSBundle(forClass:self.dynamicType)
+        let nib = UINib(nibName: "MentorMenteeView", bundle: bundle)
+        let view = nib.instantiateWithOwner(self, options: nil)[0] as! UIView
+        
+        return view
+    }
+
+}
