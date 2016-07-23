e56bbe933b7666eb30286dc03d824b3abbde7247
Yoko Yamaguchi <yamag104@mail.chapman.edu>
2016-07-23 16:31:22 -0700

    Added chat layout

---
+++ objShe/IPMQuickstart-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  IPMQuickstart-Bridging-Header.h
+//  IPMQuickstart
+//
+//  Created by Kevin Whinnery on 11/29/15.
+//  Copyright © 2015 Twilio. All rights reserved.
+//
+
+#ifndef IPMQuickstart_Bridging_Header_h
+#define IPMQuickstart_Bridging_Header_h
+
+#import <TwilioCommon/TwilioCommon.h>
+#import <TwilioIPMessagingClient/TwilioIPMessagingClient.h>
+
+#endif /* IPMQuickstart_Bridging_Header_h */
